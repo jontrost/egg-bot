acba0889612cc6edeb46baef0bf02ac0bcdff68a
Jon Trost <jontrost426@gmail.com>
2022-10-11 21:11:43 -0500

    Initial commit

---
+++ bot.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMembers
+    ] 
+});
+
+client.once('ready', () => {
+  console.log('Ready!');
+});
+
+client.on('messageCreate', async message => {
+    if (message.author.username === 'MugiwaraNoNelli') {
+        message.react('🥚');
+    }
+});
+
+client.login(process.env.TOKEN);
